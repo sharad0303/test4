fa7ff0549f9a69e52a62347b76e23cd644b9db57
sharad <sharadbxr4666@gmail.com>
2025-07-18 17:32:34 +0530

    added from 2

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
-
-                
-                boolean t= true;
-
-		int c=10;
-
+                int n=100;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
