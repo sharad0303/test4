7ba55bc28ea2b4c53c6bc210ca1816fad6aa1d09
sharad chaudhari <103510850+sharad0303@users.noreply.github.com>
2025-07-18 17:03:24 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+		int c=10;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
